eaf5832635863ba920d2959f475bafc89064e435
hermileul <hermileul5@gmail.com>
2022-12-08 04:08:22 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *
  * *main - Entry point
@@ -6,8 +7,15 @@
  * *Return: Always 0 (Success)
  *
  * */
+
+
+
 int main(void)
+
 {
+
 	  puts("\"Programming is like building a multilingual puzzle");
+
 	        return (0);
+
 }
