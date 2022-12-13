8e54dc2cc1a3721504228649917642a0408d6226
hermileul <hermileul5@gmail.com>
2022-12-13 11:58:20 -0800

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,11 @@
 #include <main.h>
 
+
 /**
- * _isalpha - checks for alphabets
+ * _isalpha - checks for alphabetic character
  * @c: character to be checked
  *
- * Return: 1 if character is a letter,0 otherwise
+ * Return: 1 if c is a letter,0 otherwise
  */
 int _isalpha(int c)
 {
