9dcc154cb3e7f2f38511b3dae7b8e2ef817b5064
hermileul <hermileul5@gmail.com>
2022-12-19 09:09:54 -0800

    task11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int pass, sum;
+
+	srand(time(null));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += pass;
+		print("%c", pass);
+	}
+
+	print("%c", 2772 - sum);
+
+	return (0);
+}
