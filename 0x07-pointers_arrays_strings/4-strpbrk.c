2e88964d1a61433dc7d91e12e0e785b05469c55e
hermileul <hermileul5@gmail.com>
2023-01-02 11:24:47 -0800

    task4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,11 +12,11 @@ char *_strpbrk(char *s, char *accept)
 {
 	int x = 0, y;
 
-	while ([x] != '\0')
+	while (s[x] != '\0')
 	{
 		for (y = 0; accept[y] != '\0'; y++)
 		{
-			if (s[x] == accepy[y])
+			if (s[x] == accept[y])
 			{
 				s = &s[x];
 				return (s);
