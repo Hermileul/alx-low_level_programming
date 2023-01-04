0c2de57899285e3e9f8cc0174827ba1f5e961f9b
hermileul <hermileul5@gmail.com>
2023-01-04 10:02:48 -0800

    task6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * is_prime_number - return 1 if is number prime
+ * @n: number to evaluate
+ * Return: 1 or 0
+ */
+int is _prime_number(int n)
+{
+	if (comparar(n, 1) == 2)
+		return (1);
+	else
+		return (0);
+}
