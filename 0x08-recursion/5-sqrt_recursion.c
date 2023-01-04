2ee9b3233d825887e50b290d2383072162f652b9
hermileul <hermileul5@gmail.com>
2023-01-04 10:50:06 -0800

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,37 @@
 #include "main.h"
-/**
- * comparar - function that returns the power of number
- * @n: original number
- * @y: counter to compare multiplication
- *
- * Return: square root
- */
-int comparar(int n, int y)
-{
-	if (y * y == n)
-	{
-		return (y);
-	}
-	else if (y * y > n)
-	{
-		return (-1);
 
+/**
+ * power_operation -  returns the natural square root of a number
+ * @n: input number.
+ * @c: iterator
+ * Return: square root or -1.
+ */
+
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
 	}
-	return (comparar(n, y + 1));
+	return (0 + power_operation(n, c + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
 }
