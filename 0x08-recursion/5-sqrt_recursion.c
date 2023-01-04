c300e9f887747878026e157d2f0afd1d234443f6
hermileul <hermileul5@gmail.com>
2023-01-04 09:54:06 -0800

    task5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * comparar - function that returns the power of number
+ * @n: original number
+ * @y: counter to compare multiplication
+ *
+ * Return: square root
+ */
+int comparar(int n, int y)
+{
+	if (y * y == n)
+	{
+		return (y);
+	}
+	else if (y * y > n)
+	{
+		return (-1);
+
+	}
+	return (compare(n, y + 1));
+}
