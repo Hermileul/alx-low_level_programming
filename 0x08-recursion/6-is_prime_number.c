13b4997a2ea86abe97066fadf0e09bace7d8e733
hermileul <hermileul5@gmail.com>
2023-01-04 10:10:22 -0800

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,21 @@
 #include "main.h"
+
 /**
- * is_prime_number - return 1 if is number prime
- * @n: number to evaluate
- * Return: 1 or 0
+ * comparar - function that returns the power of number
+ * @n: original number
+ * @y: counter to compare multiplication
+ *
+ * Return: square root
  */
-int is_prime_number(int n)
+int comparar(int n, int y)
 {
-	if (comparar(n, 1) == 2)
-		return (1);
-	else
-		return (0);
+	int count = 0;
+
+	if (y <= n)
+	{
+		if (n % y == 0)
+			count++;
+		return (count + comparar(n, y + 1));
+	}
+	return (count);
 }
