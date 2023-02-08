7c4be46df854717cdd9c0cd9e2848291ed22e1ee
hermileul <hermileul5@gmail.com>
2023-02-08 01:49:32 -0800

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * read_textile - reads a textfile and prints the contents to the POSIX STDOUT
+ * @filename: Th name of the file to read form
+ * @letters: The number of characters it should print to the STDOUT
+ * Return: Returns the number of characters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fild = 0, chk = 0;
+	char *buffer;
+
+	if (!filename || !letters)
+		return (0);
+	fild = open(filename, O_RDONLY);
+	if (fild < 0)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
+
+	close(fild);
+	free(buffer);
+	return (chik);
+}
---
+++ 0x15-file_io/main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - arguments to the compiled file
+ * @ac: arguments to be the compiled file
+ * @v:arguments to the compiled file
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	ssize_t n;
+
+	if (ac != 2)
+	{
+		dprintf(2, "usage: %s filename\n",av[0]);
+		exit(1);
+	}
+	n = read_textfile(av[1], 114);
+	printf("\n(printed chars: %li)\n", n);
+	n = read_textfile(av[1], 1024);
+	printf("\n(printed chars: %li)\n", n);
+	return (0);
+}
