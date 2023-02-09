ac7fb51857bb1d640b3876c5fa46c0590d68ab6d
hermileul <hermileul5@gmail.com>
2023-02-09 11:19:16 -0800

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcnt1.h>
-#include <unistd.\n, argc - 1);h>
+#include <unstid>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
